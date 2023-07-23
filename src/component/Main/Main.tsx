0f8f3a328a1f44a0358c276616ddef55f0a44088
myukang <myunghwan0421@gmail.com>
2023-07-24 04:34:01 +0900

    @PRETTIER 🙏

--- src/component/Main/Main.tsx
+++ src/component/Main/Main.tsx
@@ -3,11 +3,11 @@ import { modalState } from '../../atom/modal';
 import { ProfileModal } from '../Header/ProfileModal';
 
 export const Main = () => {
-	const isModalOpen = useRecoilValue(modalState);
-	return (
-		<>
-		<div className="flex absolute w-full h-full bg-slate-100"></div>
-		{ isModalOpen && <ProfileModal /> }
-		</>
-	)
-}
+  const isModalOpen = useRecoilValue(modalState);
+  return (
+    <>
+      <div className="flex absolute w-full h-full bg-slate-100"></div>
+      {isModalOpen && <ProfileModal />}
+    </>
+  );
+};
