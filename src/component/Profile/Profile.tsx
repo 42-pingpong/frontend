db37beb7f51e833b47baa40a702b70215cccab9b
Jina Kim <jinkim2@c4r2s5.42seoul.kr>
2023-08-02 16:39:01 +0900

    Profile User info로 수정

--- src/component/Profile/Profile.tsx
+++ src/component/Profile/Profile.tsx
@@ -4,15 +4,17 @@ import { GameHistory } from './GameHistory';
 import { useRecoilValue } from 'recoil';
 import { profileModalState } from '../../atom/modal';
 import { ProfileModal } from '../Header/ProfileModal';
+import { useParams } from 'react-router-dom';
 
 export const Profile = () => {
   const isProfileModalOpen = useRecoilValue(profileModalState);
+  const params = useParams();
 
   return (
     <div className="flex h-screen p-32 items-center justify-center">
       <div className="grid  grid-cols-1 lg:grid-cols-5 gap-x-32 h-full max-w-[1800px] w-screen">
         <div className="lg:col-span-2">
-          <MyProfile />
+          <MyProfile nickName={params.nickName} />
         </div>
         <div className="lg:col-span-3">
           <GameHistory />
