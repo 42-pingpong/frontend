a24e702cd9fccbebf23aab9a689a6e491c18b097
obvoso <autumninmoon@gmail.com>
2023-08-17 17:46:34 +0900

    feat: add recoil persist login state

--- src/atom/user.ts
+++ src/atom/user.ts
@@ -1,9 +1,12 @@
 import { atom } from 'recoil';
 import { UserDto } from '../interfaces/User.dto';
+import { recoilPersist } from 'recoil-persist';
 
+const { persistAtom } = recoilPersist();
 export const loginState = atom<boolean>({
   key: 'loginState',
   default: false,
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const userInfo = atom<UserDto>({
